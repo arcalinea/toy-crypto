65a02ac97acdc98f5bda71a283f65905dc237783
Jay Graber <jay@z.cash>
2017-12-15 17:14:13 -0800

    Fix ecdh

--- curve.py
+++ curve.py
@@ -1,7 +1,7 @@
 # Toy elliptic curve
 # Used https://github.com/markusju/elliptic-curves as reference
 
-class EllipticCurve(object):
+class Curve:
     # y^2 = x^3 + a*x + b
     def __init__(self, a, b, p):
         self.a = a
@@ -28,6 +28,14 @@ class EllipticCurve(object):
                 list.append(Point(x, -y % self.prime, self))
         return list
 
+    # def multiply_points(self, x, y, num):
+    #     x = x % self.prime
+    #     y = y % self.prime
+    #     spoint = Point(0, 0, self.curve)
+    #     for i in range(1, num + 1):
+    #         spoint += self
+    #     return spoint
+
 class Point:
     def __init__(self, x, y, curve):
         # Didn't we already mod y by prime above?
@@ -35,26 +43,59 @@ class Point:
         self.y = y % curve.prime
         self.curve = curve
 
-        @staticmethod
-        def invert(x, p):
-            # Fermat's
-            return pow(x, p-2, p)
+    @staticmethod
+    def invert(x, p):
+        # Fermat's
+        return pow(x, p-2, p)
 
-        def is_infinity(self):
-            return self.x <=0 and self.y <= 0
+    def is_infinity(self):
+        return self.x <=0 and self.y <= 0
 
-        def __str__(self):
-            print "Calling str"
-            return "{0}, {1}".format(str(self.x), str(self.y))
+    def __str__(self):
+        return "Point: {0}, {1}".format(str(self.x), str(self.y))
 
-        def __repr__(self):
-            return str(self)
+    def __repr__(self):
+        return str(self)
 
-        def __mul__(self, other):
-            print "Calling mul"
-            if not isinstance(other, int):
-                raise TypeError("unsup")
-            spoint = Point(0, 0, self.curve)
-            for i in range(1, other+1):
-                spoint += self
-            return spoint
+    def __mul__(self, other):
+        if not isinstance(other, int):
+            raise TypeError("unsup")
+        spoint = Point(0, 0, self.curve)
+        for i in range(1, other+1):
+            spoint += self
+        return spoint
+
+    def __add__(self, other):
+        if not isinstance(other, Point):
+            raise TypeError("unsup")
+
+        p1 = self
+        p2 = other
+        curve = self.curve
+
+        #p1 is zero
+        if p1.is_infinity():
+            return p2
+        #p2 is zero
+        if p2.is_infinity():
+            return p1
+
+        # P1=-P2
+        if p1.x == p2.x and p1.y == -p2.y % curve.prime:
+            return Point(0, 0, curve)
+
+        #Slope m of g(x) = m*x + d
+        if p1 == p2:
+            m = (3*p1.x*p1.x+curve.a) * Point.invert(2*p1.y, curve.prime)
+        else:
+            m = (p2.y-p1.y) * Point.invert(p2.x-p1.x, curve.prime)
+
+        m = m % curve.prime
+        d = (p1.y - m+p1.x) % curve.prime
+
+
+        #P3 = P1+P2
+        x3 = (pow(m,2)-p1.x-p2.x) % curve.prime
+        y3 = (m*(p1.x-x3)-p1.y) % curve.prime
+
+        return Point(x3, y3, curve)
--- ec-diffie-hellman.py
+++ ec-diffie-hellman.py
@@ -1,25 +1,24 @@
 # Toy elliptic curve diffie hellman key exchange
 
-from curve import EllipticCurve, Point
+from curve import Curve, Point
 
 class Person(object):
     def __init__(self, secret_key):
-        self.curve = EllipticCurve(65, -65, 3077783)
+        self.curve = Curve(65, -65, 3077783)
         self.generatorPoint = Point(1, 1, self.curve)
 
         self.secret_key = secret_key
         self.received = None
 
     def send(self):
-        print str(self.generatorPoint)
-        return self.secret_key * self.generatorPoint
+        return self.generatorPoint * self.secret_key
 
     def receive(self, num):
         self.received = num
         print num
 
     def compute_shared_secret(self):
-        return self.secret_key * self.received
+        return self.received * self.secret_key
 
 alice = Person(secret_key=13)
 bob = Person(secret_key=79)
